415e7527456e118e0500fd111df04aabace35f7c
Haitham El-Ghareeb <helghareeb@gmail.com>
2017-09-30 11:15:54 +0200

    Added Mansoura Lecture-03 - Data Structures and Algorithms

---
+++ data_structures_and_algorithms/Mansoura-1st-2017-2018/Lecture-03/README.md
@@ -0,0 +1,43 @@
+#بسم الله الرحمن الرحيم 
+
+## Linked Lists
+
+### Linked Lists
+https://www.tutorialspoint.com/data_structures_algorithms/linked_list_algorithms.htm
+
+- lists_01.c
+
+### Doubly Linked Lists
+https://www.tutorialspoint.com/data_structures_algorithms/doubly_linked_list_algorithm.htm
+
+- lists_02.c
+
+### Circular Linked Lists
+https://www.tutorialspoint.com/data_structures_algorithms/circular_linked_list_algorithm.htm
+
+- lists_03.c
+
+## Stack
+https://www.tutorialspoint.com/data_structures_algorithms/stack_algorithm.htm
+
+- stack_01.c
+
+## Queue
+https://www.tutorialspoint.com/data_structures_algorithms/dsa_queue.htm
+
+- queue_01.c
+
+## Trees
+
+### Binary Search Tree
+https://www.tutorialspoint.com/data_structures_algorithms/tree_data_structure.htm
+
+![Pre-Order Traversal](https://2.bp.blogspot.com/-kWTt5ZX2o_E/V5NRtAo9kmI/AAAAAAAAGrM/DLMsAdH_R2s4UUDSlDwfsxYX3I4smm0SQCLcB/s1600/preorder%2Btraversal%2Bof%2BBinary%2Btree%2Bin%2BJava.png)
+
+![In-Order Traversal](https://www.programcreek.com/wp-content/uploads/2012/12/Binary-Tree-Inorder-Traversal-in-Java.png)
+
+
+![Post-Order Traversal](http://www.manatutor.com/img/post-order-traversal.gif)
+
+- bst_01.c
+
---
+++ data_structures_and_algorithms/Mansoura-1st-2017-2018/Lecture-03/stack_01.c
@@ -0,0 +1,81 @@
+// بسم الله الرحمن الرحيم
+
+// Source: https://www.tutorialspoint.com/data_structures_algorithms/stack_program_in_c.htm
+// With modification only on main() to be
+// int main(void) to avoid the following warning from gcc
+/* warning: return type defaults to ‘int’ [-Wimplicit-int]
+ main() {
+*/
+
+#include <stdio.h>
+
+#define MAXSIZE 8
+
+int stack[MAXSIZE];     
+int top = -1;            
+
+int isempty() {
+
+   if(top == -1)
+      return 1;
+   else
+      return 0;
+}
+   
+int isfull() {
+
+   if(top == MAXSIZE)
+      return 1;
+   else
+      return 0;
+}
+
+int peek() {
+   return stack[top];
+}
+
+int pop() {
+   int data;
+	
+   if(!isempty()) {
+      data = stack[top];
+      top = top - 1;   
+      return data;
+   } else {
+      printf("Could not retrieve data, Stack is empty.\n");
+   }
+}
+
+int push(int data) {
+
+   if(!isfull()) {
+      top = top + 1;   
+      stack[top] = data;
+   } else {
+      printf("Could not insert data, Stack is full.\n");
+   }
+}
+
+int main(void) {
+   // push items on to the stack 
+   push(3);
+   push(5);
+   push(9);
+   push(1);
+   push(12);
+   push(15);
+
+   printf("Element at top of the stack: %d\n" ,peek());
+   printf("Elements: \n");
+
+   // print stack data 
+   while(!isempty()) {
+      int data = pop();
+      printf("%d\n",data);
+   }
+
+   printf("Stack full: %s\n" , isfull()?"true":"false");
+   printf("Stack empty: %s\n" , isempty()?"true":"false");
+   
+   return 0;
+}
